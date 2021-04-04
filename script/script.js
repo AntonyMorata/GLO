789324ae5b93ffbb67d17321ba6e5a6ea75a7e28
Baha Osman <bahaosman@mail.ru>
2021-04-05 00:23:33 +0200

    Исправил ошибку 02

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
 'use strict';
-alert('Hello Vlad');
 const money = 50000,
     income = 'Фриланс',
     addExpenses = 'Автосервис, интернет, спортзал',
     deposit = true, 
     mission = 1000000,
-    period = 12;
-let budgetDay = money;
+    period = 12,
+    budgetDay = money / 30;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -14,5 +13,5 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен '+ period + ' месяцев');
 console.log('Цель заработать '+ mission + ' рублей');
-console.log((addExpenses.toLowerCase()).split(', '));
-console.log(budgetDay /=30);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
