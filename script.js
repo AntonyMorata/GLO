b515bd4ca010d882fd5bc92003b2594c8038ce8c
Baha Osman <bahaosman@mail.ru>
2021-05-04 00:47:06 +0200

    Написал метод upperFirstLetter

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const appData = {
     expenses: {},
     addExpenses: [],
     deposit: false,
+    percentDeposit: 0,
+    moneyDeposit: 0,
     mission: 1000000,
     period: 3,
     budget: money,
@@ -25,13 +27,35 @@ const appData = {
     expensesMonth: 0,
 
     asking: function(){
+        let itemIncome;
+        let cashIncome;
+        if (confirm('Есть ли у вас дополнительный источник заработка?')) {
+            do {
+                itemIncome = prompt('Какой у вас дополнительный заработок?',
+                'Таксую');
+                }
+    
+            while (isNumber(itemIncome));
+            do {
+                cashIncome = +prompt('Сколько в месяц зарабатываете на этом?',
+                10000);
+                }
+            while (!isNumber(cashIncome));
+            appData.income[itemIncome] = cashIncome;
+        }   
+        
         const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 
         'Топливо, спортзал, парикмахерская');
         appData.addExpenses = addExpenses.toLowerCase().split(', ');
+        appData.addExpenses = appData.upperFirstLetter(appData.addExpenses);
         appData.deposit = confirm('Есть ли у вас депозит в банке?'); 
+        if (appData.deposit) {
+            appData.getInfoDeposit();
+        }
+        
         let amount,
             exp;
-
+        
         for (let i = 0; i < 2; i++) {
             do {
                 exp = prompt('Введите обязательную статью расходов?'); 
@@ -66,22 +90,48 @@ const appData = {
         } else if (appData.budgetDay === 0) {
             return ('Бюджет за день равен нулю');
         }
-
     },
+    getInfoDeposit: function(){
+        do {
+            if (appData.deposit){
+                appData.percentDeposit = +prompt('Какой годовой процент?', '10');
+            } 
+        }
+        while (!isNumber(appData.percentDeposit));
+        do {
+            if (appData.deposit){
+                appData.moneyDeposit = +prompt('Какая сумма заложена?', '10000');
+            } 
+        }
+        while (!isNumber(appData.moneyDeposit));
+    },
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
+    },
+    upperFirstLetter: function(array) {
+        let word = '';
+        for (let i = 0; i < array.length; i++) {
+            word += array[i][0].toUpperCase() + array[i].slice(1) + ', ';
+        }
+        return word.slice(0, -2);
+    }
 };
-
 
 appData.asking();
 appData.getExpensesMonth();
 appData.getBudget();
 appData.getTargetMonth();
 
-console.log(appData);
+
+console.log('Возможные расходы: ' + appData.addExpenses);
 console.log('Расходы за месяц: '+ appData.expensesMonth + ' рублей');
 console.log('Цель будет достигнута: '+ appData.period + ' месяцев');
 console.log(appData.getStatusIncome());
 console.log('Наша программа включает в себя данные:');
 for (let key in appData) {
-    
     console.log('свойства: '+ key + ' значение: ' + appData[key]);
 }
+
+
+
+
